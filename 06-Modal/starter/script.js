eda8ab86a61756aae119ff9849245c8eceb08ea6
Nick Chikhradze <136087880+nikoloz100@users.noreply.github.com>
2025-07-12 14:11:04 -0400

    06-modal
    - created popup modal with jacascript class manipulation;
    - modified files listed below accordingly;
    
    	modified:   script.js

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+const toggleHidden = function () {
+  modal.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+};
+
+const keyClose = function (event) {
+  event.preventDefault();
+  if (event.code === 'Escape' && !modal.classList.contains('hidden')) {
+    toggleHidden();
+  }
+};
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', toggleHidden);
+  btnsShowModal[i].addEventListener('click', function () {
+    this.blur();
+  });
+}
+
+btnCloseModal.addEventListener('click', toggleHidden);
+
+overlay.addEventListener('click', toggleHidden);
+
+document.addEventListener('keydown', keyClose);
