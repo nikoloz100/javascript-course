7efecc71361e8499d282946692e3ba987b16b0e1
Nick Chikhradze <136087880+nikoloz100@users.noreply.github.com>
2025-07-13 11:01:02 -0400

    07 - pig game
    - stored dom elements to local variables;
    - defined starting conditions to track records and future
    reset functionality;
    - created rolling dice function for player one;
    - switch player functionality should be created;
    
    	modified:   index.html
    	modified:   script.js
    	modified:   style.css

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+// Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+//Rollingf Dice Functionality
+
+btnRoll.addEventListener('click', function () {
+  // 1. Generating random dice roll number
+
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display number on dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `./dice-${dice}.png`;
+
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // if no add to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //TODO change later to switch users
+    // if true, switch to next player
+  }
+});
